dbf847e35ffedcd043fd78946d9b1f728f2ad37f
Vansh <vanshgarg32@gmail.com>
2023-08-22 13:23:43 +0530

    Routing basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginModalComponent } from './login-modal/login-modal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '#', pathMatch: 'full' },
+  { path: 'login', component:LoginModalComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
